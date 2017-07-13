a3571dbf30e8a2830b001eb9e07b0b19c5d17d7c
Johns, Robert <rjohns@visa.com>
2017-07-13 16:02:17 -0500

    Initial commit

---
+++ AdamPodTestApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AdamPodTestApp
+//
+//  Created by Johns, Robert on 7/11/17.
+//  Copyright © 2017 Visa. All rights reserved.
+//
+
+import UIKit
+import AdamPodTest
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button = MyButton()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'AdamPodTestApp' do
+  use_frameworks!
+  pod 'AdamPodTest', '0.1.6'
+end
